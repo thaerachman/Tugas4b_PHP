783a39d65917f8649f8c35eae3bcd256342f922a
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-07 15:41:44 +0700

    Add files via upload

---
+++ tugas_4_b.php
@@ -0,0 +1,17 @@
+<?php
+echo "Tugas 4b php <br>";
+echo "------------------ <br>";
+$jalur=523;
+$terpakai=8891;
+
+// Rumus:
+$rata2_pakai=$terpakai / $jalur;
+
+echo "Jumlah Jalur : ".$jalur;
+echo "<br>";
+echo "Jumlah terpakai : ".$terpakai." Liter";
+echo "<br>";
+
+echo "Rata - Rata Pemakaian: ". $rata2_pakai." Liter/keluarga";
+
+?>
